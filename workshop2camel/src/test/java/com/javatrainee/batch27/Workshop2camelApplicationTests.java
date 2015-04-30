2f0cf5c883e589fb61f15adbd55e36cbdd158c3e
Sarasuartha <sarasuartha@sigma.co.id>
2015-04-30 19:37:59 +0800

    Starter Camel

---
+++ workshop2camel/src/test/java/com/javatrainee/batch27/Workshop2camelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javatrainee.batch27;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Workshop2camelApplication.class)
+public class Workshop2camelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
